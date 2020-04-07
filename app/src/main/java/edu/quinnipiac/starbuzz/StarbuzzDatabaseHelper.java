341b430441c5c71575319fd2ab71d626feb0cdeb
KatieRosell <32073902+KatieRosell@users.noreply.github.com>
2020-04-07 18:57:07 -0400

    Final Comitt
    completed with SQLite database implementation

---
+++ app/src/main/java/edu/quinnipiac/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,60 @@
+package edu.quinnipiac.starbuzz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz";//name of our database
+    private static final int DB_VERSION = 2; //version of the database
+
+    public StarbuzzDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateMyDatabase(db, 0, DB_VERSION);
+
+        /**
+        db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                +  "NAME TEXT, " + "DESCRIPTION TEXT," + "IMAGE_RESOURCE_ID INTEGER);");
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drop coffee", R.drawable.filter);
+         **/
+
+    }
+
+    private void insertDrink(SQLiteDatabase db, String name, String description, int resourceID) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceID);
+        db.insert("DRINK", null, drinkValues);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldversion, int newversion){
+        if (oldversion < 1){
+            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    +  "NAME TEXT, " + "DESCRIPTION TEXT," + "IMAGE_RESOURCE_ID INTEGER);");
+            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+            insertDrink(db, "Filter", "Our best drop coffee", R.drawable.filter);
+        }
+        if(oldversion < 2){
+            //Code to add the extra column
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
+    }
+
+
+
+}
